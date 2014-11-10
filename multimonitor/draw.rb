a18f82def08021131e8737902112dda439b0bc1c
Yuuki Harano <masm@masm11.ddo.jp>
2014-11-10 19:40:43 +0900

    描画をグラフの領域に限定。領域外に書き込むと落ちることがあった。

--- multimonitor/draw.rb
+++ multimonitor/draw.rb
@@ -16,12 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+def clip(val, min, max)
+  val = min if val < min
+  val = max if val > max
+  val
+end
+
 def draw_line(pixbuf, x, y1, y2, r, g, b)
 #  p 'draw_line'
   rowstride = pixbuf.rowstride
   pixels = pixbuf.pixels;
   
-  for y in y1.to_i..y2.to_i
+  y1 = y1.to_i
+  y2 = y2.to_i
+  if y1 > y2
+    t = y1
+    y1 = y2
+    y2 = t
+  end
+  x = clip(x, 0, pixbuf.width - 1)
+  y1 = clip(y1, 0, pixbuf.height - 1)
+  y2 = clip(y2, 0, pixbuf.height - 1)
+  
+  for y in y1..y2
     p = y * rowstride + x * 3
     pixels[p..p+2] = [ r, g, b ].pack('C3')
   end
