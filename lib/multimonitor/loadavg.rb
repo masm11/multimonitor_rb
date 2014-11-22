7dffd589ba311f55ba36de6861139e6842598688
Yuuki Harano <masm@masm11.ddo.jp>
2014-11-22 11:24:33 +0900

    gem化。

--- lib/multimonitor/loadavg.rb
+++ lib/multimonitor/loadavg.rb
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require_relative 'draw'
-require_relative 'color'
+require 'multimonitor/draw'
+require 'multimonitor/color'
 
 class LoadAvg
   def initialize(dev)
--- lib/multimonitor/network.rb
+++ lib/multimonitor/network.rb
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require_relative 'draw'
-require_relative 'color'
+require 'multimonitor/draw'
+require 'multimonitor/color'
 require 'network_interface'
 
 class Network
