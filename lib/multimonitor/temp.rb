5679a7194bffe67a4e144eaba1ce408c6a667930
Yuuki Harano <masm@masm11.ddo.jp>
2016-03-25 21:20:47 +0900

    coretemp を名前指定にした。

--- lib/multimonitor/temp.rb
+++ lib/multimonitor/temp.rb
@@ -23,12 +23,26 @@ require 'multimonitor/color'
 class Temp
   def initialize(dev)
     @dev = dev
-    @id1, @id2, @id3 = dev.split('-')
+    @id1, @id2n, @id3 = dev.split('-')
 
     @data = []
     @max = nil
+    @id2 = -1
     
     begin
+      Dir.glob("/sys/devices/platform/coretemp.#{@id1}/hwmon/hwmon*") do |dir|
+        File.open("#{dir}/name") do |f|
+          f.each_line do |line|
+            line.chomp!
+            if line == @id2n
+              if /(\d+)$/ =~ dir
+                @id2 = $1
+              end
+            end
+          end
+        end
+      end
+
       File::open("/sys/devices/platform/coretemp.#{@id1}/hwmon/hwmon#{@id2}/temp#{@id3}_max") do |f|
         f.each_line do |line|
           @max = line.to_i / 1000.0
@@ -77,7 +91,7 @@ class Temp
   end
 
   def get_label
-    "Temperature\n#{@dev}"
+    "Temperature\n#{@id1}-#{@id2}-#{@id3}"
   end
   
   def get_tick_per_draw
